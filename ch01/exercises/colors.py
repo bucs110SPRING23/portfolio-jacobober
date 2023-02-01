c14666a47a3bf01cc6e958dc13598591d761ae13
Jacob Ober <jober1@binghamton.edu>
2023-02-01 18:07:04 -0500

    Colors commit

--- ch01/exercises/colors.py
+++ ch01/exercises/colors.py
@@ -4,6 +4,13 @@ print("I have ", len(colors), " colors to choose from.")
 print(colors[0])
 print(colors[1])
 print(colors[2])
+print(colors[3])
+print(colors[4])
+print(colors[5])
+print(colors[6])
+print(colors[7])
+print(colors[8])
+print(colors[9])
 
 # Runtime Operations
 'var = input("prompt")'
