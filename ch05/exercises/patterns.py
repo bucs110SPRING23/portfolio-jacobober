e0a0ed367aac5b339a7e2882adbed391c6a5cdde
Jacob Ober <jober1@binghamton.edu>
2023-03-06 18:05:34 -0500

    StarPyramid

---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,8 @@
+stars = int(input("Enter how many rows you want: "))
+def star_pyramid():
+    for i in range(1, stars + 1):
+        print("*" * i)
+star_pyramid()
+    
+    # for r in range(stars):
+    #     print("*")
